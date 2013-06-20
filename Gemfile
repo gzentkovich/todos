5ecda0234c57d407ebd84a77c9c2e047ce60292c
Grandslam <grandslam2008@gmail.com>
2013-06-19 14:22:07 -1000

    initial commit, rspec install

---
+++ Gemfile
@@ -0,0 +1,22 @@
+source 'https://rubygems.org'
+
+gem 'rails', '3.2.13'
+gem 'sqlite3'
+gem 'jquery-rails'
+gem 'high_voltage'
+
+group :development, :test do
+  gem 'rspec-rails'
+  gem 'capybara', '1.1.4'
+end
+
+group :test do
+  gem 'factory_girl_rails', '4.1.0'
+end
+
+group :assets do
+  gem 'sass-rails',   '3.2.5'
+  gem 'coffee-rails', '3.2.2'
+  gem 'uglifier', '1.2.3'
+end
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Todos::Application.config.secret_token = '378784458898a12889a41f3df42fb8df3f8de7a86892186d14ec0c0f8cc9a24b26059d430fe09b34e2adaac68e5b5afc6f12dfd2a58f50a0bf0eb205c440a1dc'
